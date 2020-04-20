785702592244f0f56ad06916808a963662223c8d
Nicolas Vanheuverzwijn <nicolas.vanheu@gmail.com>
2020-04-20 17:18:42 -0400

    bastion: honor ig fields

--- pkg/model/bootstrapscript.go
+++ pkg/model/bootstrapscript.go
@@ -130,10 +130,6 @@ func (b *BootstrapScript) buildEnvironmentVariables(cluster *kops.Cluster) (map[
 // ResourceNodeUp generates and returns a nodeup (bootstrap) script from a
 // template file, substituting in specific env vars & cluster spec configuration
 func (b *BootstrapScript) ResourceNodeUp(ig *kops.InstanceGroup, cluster *kops.Cluster) (*fi.ResourceHolder, error) {
-	// Bastions can have AdditionalUserData, but if there isn't any skip this part
-	if ig.IsBastion() && len(ig.Spec.AdditionalUserData) == 0 {
-		return nil, nil
-	}
 
 	functions := template.FuncMap{
 		"NodeUpSource": func() string {
